9f31b6df53172a25c5048ea0c776c95527e38855
Alby <alby4645@gmail.com>
2024-12-28 15:12:55 +0530

    6 commit

--- src/Controllers/authRoutes.js
+++ src/Controllers/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel"); // Import the User model
 const { authenticateToken } = require("../middleware/authMiddleware.js");
