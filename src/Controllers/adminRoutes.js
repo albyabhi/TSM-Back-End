edc00d68faf7b3479c3e2ad77971fc70d7673083
Alby <alby4645@gmail.com>
2024-12-28 15:15:56 +0530

    7 commit

--- src/Controllers/adminRoutes.js
+++ src/Controllers/adminRoutes.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const express = require('express'); // don't forget to import express
 const { authenticateAdminToken } = require('../middleware/authMiddleware.js');
