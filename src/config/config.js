74e2e05390e29a238a84396775892dc0ce29e72d
Alby <alby4645@gmail.com>
2024-12-28 15:02:08 +0530

    4 commit

--- src/config/config.js
+++ src/config/config.js
@@ -1,23 +1,28 @@
+require('dotenv').config();  // Load environment variables
+
+const jwt = require('jsonwebtoken');
+
 const generateToken = (user) => {
+  if (!user || !user._id || !user.username || !user.email) {
+    throw new Error('User data is incomplete');
+  }
+
   const payload = {
     userId: user._id,
     username: user.username,
     email: user.email,
   };
 
+  const expiresIn = '1h';  // Expiration time for the JWT token
 
-  // Specify an expiration time if needed
-  const expiresIn = '1h';
-
-  // Generate the JWT token
-  const token = jwt.sign(payload, secretKey, { expiresIn });
+  const token = jwt.sign(payload, process.env.SECRET_KEY, { expiresIn });
 
   return token;
 };
 
 module.exports = {
   generateToken,
-  mongoURI: 'mongodb+srv://Albyabhi:Albyabhi4645@cluster0.prdylvt.mongodb.net/TSM?retryWrites=true&w=majority',
+  mongoURI: process.env.MONGO_URI,
   port: process.env.PORT || 5000,
-  secretKey : 'g^b25s9#L!aD2sF7cP4%kR1@uA8',
+  secretKey: process.env.SECRET_KEY,
 };
